641c677a2a9f883644f4425552bba19770284f7e
Sophia <111466302+MintyLuve@users.noreply.github.com>
2024-03-12 10:35:13 -0400

    Added main page, fonts, owl, made landscape

--- app/src/main/java/com/example/flappyowl/MainActivity.java
+++ app/src/main/java/com/example/flappyowl/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.flappyowl;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        next = findViewById(R.id.next);
+
+        next.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), GameActivity.class)));
     }
 }
